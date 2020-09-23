3106c23e280cdb7b638532265db823fcc80f5cb7
brandondjango <brandonlockridge@gmail.com>
2020-09-23 14:53:35 -0400

    sumlator additions

--- lib/driver_helper.rb
+++ lib/driver_helper.rb
@@ -12,12 +12,28 @@ class DriverHelper
       options["caps"]["avd"] = avd
 
       #create and start driver
-      driver = Appium::Driver.new(options, true)
-      driver.start_driver
+      @driver = Appium::Driver.new(options, true)
+      @driver.start_driver
 
       #return Appium driver
-      return driver
+      return @driver
     end
+
+    def create_android_chrome_emulator(browserName:, avd:)
+          #pull initial capabilities from template
+          options = YAML.load(File.read(File.join(__dir__, "driver_templates/android_emulator.yml")))
+
+          #populate capabilities with arguments
+          options["caps"]["browserName"] = browserName
+          options["caps"]["avd"] = avd
+
+          #create and start driver
+          @driver = Appium::Driver.new(options, true)
+          @driver.start_driver
+
+          #return Appium driver
+          return @driver
+        end
 
     def create_android_real_device(app:, deviceName:)
       #pull initial capabilities from template
@@ -28,14 +44,31 @@ class DriverHelper
       options["caps"]["deviceName"] = deviceName
 
       #create and start driver
-      driver = Appium::Driver.new(options, true)
-      driver.start_driver
+      @driver = Appium::Driver.new(options, true)
+      @driver.start_driver
 
       #return Appium driver
-      return driver
+      return @driver
     end
 
-    def create_ios_simulator(app:, platformVersion:, deviceName:)
+    def create_ios_simulator_safari(browserName:, platformVersion:, deviceName:)
+      #pull initial capabilities from template
+      options = YAML.load(File.read(File.join(__dir__, "driver_templates/ios_simulator.yml")))
+
+      #populate capabilities with arguments
+      options["caps"]["browserName"] = browserName
+      options["caps"]["platformVersion"] = platformVersion
+      options["caps"]["deviceName"] = deviceName
+
+      #create and start driver
+      @driver = Appium::Driver.new(options, true)
+      @driver.start_driver
+
+      #return Appium driver
+      return @driver
+    end
+
+    def create_ios_simulator_app(app:, platformVersion:, deviceName:)
       #pull initial capabilities from template
       options = YAML.load(File.read(File.join(__dir__, "lib/driver_templates/ios_simulator.yml")))
 
@@ -45,29 +78,11 @@ class DriverHelper
       options["caps"]["deviceName"] = deviceName
 
       #create and start driver
-      driver = Appium::Driver.new(options, true)
-      driver.start_driver
+      @driver = Appium::Driver.new(options, true)
+      @driver.start_driver
 
       #return Appium driver
-      return driver
+      return @driver
     end
-
-    def create_ios_real_device(app:, platformVersion:, udid:)
-      #pull initial capabilities from template
-      options = YAML.load(File.read(File.join(__dir__, "lib/driver_templates/ios_simulator.yml")))
-
-      #populate capabilities with arguments
-      options["caps"]["app"] = app
-      options["caps"]["platformVersion"] = platformVersion
-      options["caps"]["udid"] = udid
-
-      #create and start driver
-      driver = Appium::Driver.new(options, true)
-      driver.start_driver
-
-      #return Appium driver
-      return driver
-    end
-
 
 end
--- lib/irb_env.rb
+++ lib/irb_env.rb
@@ -4,3 +4,13 @@ require 'appium_lib'
 
 #require BasePage and DriverHelper
 require_rel './'
+
+def create_iphonex_safari_driver
+    helper = DriverHelper.new
+    helper.create_ios_simulator_safari(browserName: "Safari", platformVersion: "13.5", deviceName: "iPhone X")
+end
+
+def create_pixel_chrome_driver
+    helper = DriverHelper.new
+    helper.create_android_chrome_emulator(browserName: "chrome", avd: "Pixel3")
+end
